0dbffa256508528e87c57b6a42c72b3fdb6f84e6
no2ca <taiseiono6@gmail.com>
2026-08-30 05:09:44 +0900

    refactor: 長い処理を関数に整理

--- ipv4.go
+++ ipv4.go
@@ -27,7 +27,7 @@ func ParseIPv4Header(buf []byte) (IPv4Header, error) {
 	if hdr.Version != 4 {
 		return IPv4Header{}, fmt.Errorf("not an IPv4 packet: version=%d", hdr.Version)
 	}
-	
+
 	hdr.IHL = buf[0] & 0x0f
 	if hdr.IHL < 5 {
 		return IPv4Header{}, fmt.Errorf("invalid IHL: %d", hdr.IHL)
@@ -39,14 +39,14 @@ func ParseIPv4Header(buf []byte) (IPv4Header, error) {
 
 	hdr.TotalLen = binary.BigEndian.Uint16(buf[2:4])
 	if int(hdr.TotalLen) > len(buf) {
-    return IPv4Header{}, fmt.Errorf("total length %d exceeds buffer size %d", hdr.TotalLen, len(buf))
+		return IPv4Header{}, fmt.Errorf("total length %d exceeds buffer size %d", hdr.TotalLen, len(buf))
 	}
 
 	hdr.Protocol = buf[9]
 	hdr.Checksum = binary.BigEndian.Uint16(buf[10:12])
 	hdr.Src = netip.AddrFrom4([4]byte(buf[12:16]))
 	hdr.Dst = netip.AddrFrom4([4]byte(buf[16:20]))
-	
+
 	if calculateChecksum(buf[:headerLen]) != 0 {
 		return IPv4Header{}, fmt.Errorf("checksum mismatch")
 	}
@@ -57,7 +57,7 @@ func ParseIPv4Header(buf []byte) (IPv4Header, error) {
 // calculateChecksum computes the IPv4 header checksum (RFC 791).
 func calculateChecksum(buf []byte) uint16 {
 	var sum uint32
-	
+
 	for i := 0; i+1 < len(buf); i += 2 {
 		sum += uint32(binary.BigEndian.Uint16((buf[i : i+2])))
 	}
@@ -65,10 +65,10 @@ func calculateChecksum(buf []byte) uint16 {
 	if len(buf)%2 == 1 {
 		sum += uint32(buf[len(buf)-1]) << 8
 	}
-	
+
 	for sum > 0xffff {
 		sum = (sum & 0xffff) + (sum >> 16)
 	}
-	
+
 	return ^uint16(sum)
 }
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"io"
 	"log"
 	"os/signal"
 	"syscall"
@@ -24,32 +25,61 @@ func main() {
 
 	fmt.Printf("created tun interface: %s\n", name)
 
-	go func() {
-		defer fmt.Printf("Reading packets done. Waiting for an interrupt...")
-		buf := make([]byte, 1500)
-		for {
-			select {
-			case <-ctx.Done():
-				return
-			default:
-				n, err := tun.Read(buf)
-				if err != nil {
-					log.Print(err)
-					return
-				}
+	ch := make(chan readResult)
+	go readLoop(ctx, ch, tun)
 
-				fmt.Printf("========\n")
-				fmt.Printf("received %d bytes: %  x\n", n, buf[:n])
-
-				hdr, err := ParseIPv4Header(buf)
-				if err != nil {
-					log.Print(err)
-				}
-				// IPv4: 1=ICMP, 6=TCP, 17=UDP
-				fmt.Printf("Protocol: %v, Src: %v, Dst: %v\n", hdr.Protocol, hdr.Src, hdr.Dst)
-			}
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case res := <-ch:
+			displayPacket(res)
 		}
-	}()
+	}
+}
 
-	<-ctx.Done()
+type readResult struct {
+	n   int
+	buf []byte
+}
+
+func readLoop(ctx context.Context, ch chan readResult, tun io.Reader) {
+	defer fmt.Printf("Reading packets done. Waiting for an interrupt...")
+	buf := make([]byte, 1500)
+	for {
+		n, err := tun.Read(buf)
+		select {
+		case ch <- readResult{n: n, buf: buf}:
+		case <-ctx.Done():
+			return
+		}
+
+		if err != nil {
+			log.Print(err)
+			return
+		}
+	}
+}
+
+func displayPacket(res readResult) {
+	fmt.Printf("========\n")
+	fmt.Printf("received %d bytes: %  x\n", res.n, res.buf[:res.n])
+
+	hdr, err := ParseIPv4Header(res.buf)
+	if err != nil {
+		log.Print(err)
+	}
+	// IPv4: 1=ICMP, 6=TCP, 17=UDP
+	var proto string
+	switch hdr.Protocol {
+	case 1:
+		proto = "ICMP"
+	case 6:
+		proto = "TCP"
+	case 17:
+		proto = "UDP"
+	default:
+		proto = "other"
+	}
+	fmt.Printf("Protocol: %v (%s), Src: %v, Dst: %v\n", hdr.Protocol, proto, hdr.Src, hdr.Dst)
 }
