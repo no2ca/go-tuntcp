f40179e77a14600dca402ec0b44ab1c847570b3b
no2ca <taiseiono6@gmail.com>
2026-09-08 00:29:20 +0900

    tcp: Transmission Control Blockの定義

--- tcp.go
+++ tcp.go
@@ -142,3 +142,28 @@ func (s State) String() string {
 		return ""
 	}
 }
+
+// SendSequenceSpace は RFC 9293 3.3.1 の Send Sequence Variables
+type SendSequenceSpace struct {
+	UNA uint32 // 未確認の最古のシーケンス番号
+	NXT uint32 // 次に送信するシーケンス番号
+	WND uint16 // 相手が広告した受信ウィンドウ
+	UP  bool   // urgent pointer
+	WL1 uint32 // 最後にウィンドウ更新を行ったセグメントの SEG.SEQ
+	WL2 uint32 // 同じく SEG.ACK
+	ISS uint32 // 初期送信シーケンス番号
+}
+
+// RecvSequenceSpace は RFC 9293 3.3.1 の Receive Sequence Variables
+type RecvSequenceSpace struct {
+	NXT uint32 // 次に受信を期待するシーケンス番号
+	WND uint16 // 自分が広告している受信ウィンドウ
+	UP  bool   // urgent pointer
+	IRS uint32 // 初期受信シーケンス番号
+}
+
+type ControlBlock struct {
+	State State
+	Snd   SendSequenceSpace
+	Rcv   RecvSequenceSpace
+}
